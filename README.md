6e8b92e0667ff04c403f4efcb7842b6ced086372
Fayland Lam <fayland@gmail.com>
2016-06-12 12:11:33 +0800

    with milla new

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+MojoX::Log::Syslog - Blah blah blah
+
+# SYNOPSIS
+
+    use MojoX::Log::Syslog;
+
+# DESCRIPTION
+
+MojoX::Log::Syslog is
+
+# AUTHOR
+
+Fayland Lam <fayland@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2016- Fayland Lam
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
--- lib/MojoX/Log/Syslog.pm
+++ lib/MojoX/Log/Syslog.pm
@@ -1,5 +1,39 @@
-use strict;
-use warnings;
 package MojoX::Log::Syslog;
 
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MojoX::Log::Syslog - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use MojoX::Log::Syslog;
+
+=head1 DESCRIPTION
+
+MojoX::Log::Syslog is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2016- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
